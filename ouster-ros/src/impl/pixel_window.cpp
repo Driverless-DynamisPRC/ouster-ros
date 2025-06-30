98abe8b3c320d1597df526fd655fc015b94990b6
AlessandroBiaggi <alessandro1.biaggi@mail.polimi.it>
2025-07-01 00:25:58 +0200

    Update pixel window calculation

--- ouster-ros/src/impl/pixel_window.cpp
+++ ouster-ros/src/impl/pixel_window.cpp
@@ -1,25 +1,44 @@
 #include "pixel_window.h"
 
 #include <algorithm>
+#include <stdexcept>
 
 namespace ouster {
 
-    std::pair<uint64_t, uint64_t> horizon_to_pixel_window(const std::vector<double>& beam_altitude_angles,
+    std::pair<uint64_t, uint64_t> horizon_to_pixel_window(const std::vector<double> &beam_altitude_angles,
                                                           int64_t horizon_window_start,
                                                           int64_t horizon_window_end) {
-        auto pixel_start_it = std::find_if(
-            beam_altitude_angles.rbegin(), beam_altitude_angles.rend(),
-            [&](const auto &angle) { return angle >= horizon_window_start; });
-        auto pixel_end_it = std::find_if(
-            pixel_start_it + 1, beam_altitude_angles.rend(),
-            [&](const auto &angle) { return angle >= horizon_window_end; });
+        if (beam_altitude_angles.empty())
+            throw std::invalid_argument("horizon_to_pixel_window: beam_altitude_angles is empty");
 
-        auto pixel_start = static_cast<uint64_t>(std::distance(
-            beam_altitude_angles.rbegin(), pixel_start_it));
-        auto pixel_end = static_cast<uint64_t>(std::distance(
-            beam_altitude_angles.rbegin(), pixel_end_it));
+        std::pair<uint64_t, uint64_t> pixel_window;
 
-        return {pixel_start, pixel_end};
+        const auto hw_start = static_cast<double>(horizon_window_start) * 1e-3;
+        const auto hw_end = static_cast<double>(horizon_window_end) * 1e-3;
+
+        if (beam_altitude_angles.front() < beam_altitude_angles.back()) {
+            const auto pixel_start_it = std::lower_bound(
+                    beam_altitude_angles.begin(), beam_altitude_angles.end(), hw_start);
+            const auto pixel_end_it = std::upper_bound(
+                    pixel_start_it, beam_altitude_angles.end(), hw_end);
+
+            pixel_window.first = static_cast<uint64_t>(std::distance(
+                    beam_altitude_angles.begin(), pixel_start_it));
+            pixel_window.second = static_cast<uint64_t>(std::distance(
+                    beam_altitude_angles.begin(), pixel_end_it));
+        } else {
+            const auto pixel_start_it = std::lower_bound(
+                beam_altitude_angles.rbegin(), beam_altitude_angles.rend(), hw_start);
+            const auto pixel_end_it = std::upper_bound(
+                    pixel_start_it + 1, beam_altitude_angles.rend(), hw_end);
+
+            pixel_window.first = static_cast<uint64_t>(std::distance(
+                    beam_altitude_angles.rbegin(), pixel_start_it));
+            pixel_window.second = static_cast<uint64_t>(std::distance(
+                    beam_altitude_angles.rbegin(), pixel_end_it));
+        }
+
+        return pixel_window;
     }
 
-}  // namespace ouster
+}// namespace ouster
